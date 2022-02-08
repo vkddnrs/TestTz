68c15f508adb369c5ec51a3d92b8aa9211f3c0ed
vkddnrs <vkddnrs@gmail.com>
2022-02-08 13:04:30 +0300

    Health and Satiety components was added and setted.

---
+++ Source/TestTz/Public/Parameters/HealthParameterComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Parameters/BaseParameterComponent.h"
+#include "HealthParameterComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTTZ_API UHealthParameterComponent : public UBaseParameterComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/TestTz/Public/Parameters/SatietyParameterComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Parameters/BaseParameterComponent.h"
+#include "SatietyParameterComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTTZ_API USatietyParameterComponent : public UBaseParameterComponent
+{
+	GENERATED_BODY()
+	
+};
